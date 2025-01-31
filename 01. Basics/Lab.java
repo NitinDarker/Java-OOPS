f5502d95be6a7c876c9b572b069bae9e465aa6f5
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-01-31 18:20:51 +0530

    Adding Assignment 2

--- 01. Basics/Lab.java
+++ 01. Basics/Lab.java
@@ -1,27 +1,20 @@
-class Class {
+class Classic {
     int a, b;
 }
 
 class Lab{
     public static void main(String args[]) {
+
         int x = 128;
         byte b = (byte)x;
         System.out.println(b);
         System.out.println(x);
-        Class ob = new Class();
-        boolean bb = ob instanceof Class;
+
+        Classic ob = new Classic();
+        boolean bb = ob instanceof Classic;
         System.out.println(bb);
-        int longvar = 5;
-        switch (longvar) {
-            case 'a':
-                System.out.println("hi");
-                break;
-        
-            default:
-                break;
-        }
+        System.out.println(ob.getClass());
     }
     // instanceof
     // ob.getclass()
-
 }
